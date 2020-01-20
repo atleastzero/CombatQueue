3e66542169c4542aa3118cb589024e5b8cc03ef1
megan <megan.obryan52@gmail.com>
2020-01-19 18:03:38 -0800

    Renamed some things in app.py, added render_template import

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/characters')
 def show_characters():
-    return render_template("show_characters.html")
+    return render_template("characters_show.html")
 
 @app.route('/addCharacter')
 def create_character():
-    return render_template("create_character.html")
+    return render_template("character_new.html")
