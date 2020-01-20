9dfa40b868146549c0c14312049324f84f3265fa
megan <megan.obryan52@gmail.com>
2020-01-19 17:19:40 -0800

    Ordered fighters

--- fight.py
+++ fight.py
@@ -1,5 +1,3 @@
-
-
 playerCharacters = ["Jenna", "Karen", "KeyboardInterupt"]
 enemyCharacters = ["A seal", "An angel"]
 fighters = []
@@ -8,10 +6,19 @@ def main():
     print("Hostility detected! Who's fighting?")
 
     for char in playerCharacters:
-        if input(char+ "? [y/n]") == 'y':
-            number = int(input("What's their initiative?"))
+        if input(char + "? [y/n]: ") == 'y':
+            number = int(input("What's their initiative?: "))
             fighters.append((number, char))
+
+    for char in enemyCharacters:
+        number = int(input("What's " + char + "'s initiative?: "))
+        fighters.append((number, char))
+
     print(fighters)
 
+    print("Ordered fighters:")
+    fighters.sort()
+    print(fighters)
+    
 if __name__ == "__main__":
-    main()
+    main() 
